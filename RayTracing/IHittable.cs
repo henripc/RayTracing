9ab4d9ee6c036be24dd31aa19e24dd912bfb7863
henripc <henriquechimetta@outlook.com>
2022-07-14 11:44:49 -0300

    Removes Material and Hittable classes.
    Removed Material and Hittable classes.
    Added IMaterial and IHittable interfaces.

---
+++ RayTracing/IHittable.cs
@@ -0,0 +1,18 @@
+﻿namespace RayTracing
+{
+    /// <summary>
+    /// <see langword="Interface"/> that represents a object that can be hit by a <see cref="Ray"/>.
+    /// </summary>
+    public interface IHittable
+    {
+        /// <summary>
+        /// Represents a <see cref="Ray"/> hit in the object.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="tMin"></param>
+        /// <param name="tMax"></param>
+        /// <param name="rec"></param>
+        /// <returns>A <see cref="bool"/> indicating if the object was hit.</returns>
+        bool Hit(Ray r, double tMin, double tMax, HitRecord rec);
+    }
+}
---
+++ RayTracing/IMaterial.cs
@@ -0,0 +1,20 @@
+﻿using Color = RayTracing.Vec3;
+
+namespace RayTracing
+{
+    /// <summary>
+    /// <see langword="interface"/> that represents a material.
+    /// </summary>
+    public interface IMaterial
+    {
+        /// <summary>
+        /// Method that verify if a <see cref="Ray"/> is scaterred when hitting a object.
+        /// </summary>
+        /// <param name="rIn"></param>
+        /// <param name="rec"></param>
+        /// <param name="attenuation"></param>
+        /// <param name="scattered"></param>
+        /// <returns>A <see langword="bool"/> indicating if the <see cref="Ray"/> was scattered.</returns>
+        bool Scatter(Ray rIn, HitRecord rec, Color attenuation, Ray scattered);
+    }
+}
