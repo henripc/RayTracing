2614d527229480a27518afe0d1f565a39276f965
henripc <henriquechimetta@outlook.com>
2022-07-14 11:47:31 -0300

    Finished Camera class.

--- RayTracing/Camera.cs
+++ RayTracing/Camera.cs
@@ -1,42 +1,64 @@
 ﻿using System;
 
+using Point3 = RayTracing.Vec3;
+
 namespace RayTracing
 {
+    /// <summary>
+    /// <see langword="class"/> that represents the camera point of view.
+    /// </summary>
     public class Camera
     {
         private readonly Point3 _origin;
         private readonly Vec3 _lowerLeftCorner;
         private readonly Vec3 _horizontal;
         private readonly Vec3 _vertical;
-        private readonly Vec3 u;
-        private readonly Vec3 v;
-        private readonly Vec3 w;
-        private readonly double lensRadius;
+        private readonly Vec3 _u;
+        private readonly Vec3 _v;
+        private readonly Vec3 _w;
+        private readonly double _lensRadius;
 
-        public Camera(Point3 lookFrom, Point3 lookAt, Vec3 vUp, double vfov,
-                      double aspectRatio, double aperture, double focusDist)  // vfov = vertical field-of-view in degrees
+        /// <summary>
+        /// Instanciate a new <see cref="Camera"/> object.
+        /// </summary>
+        /// <param name="lookFrom"></param>
+        /// <param name="lookAt"></param>
+        /// <param name="vUp"></param>
+        /// <param name="vfov"></param>
+        /// <param name="aspectRatio"></param>
+        /// <param name="aperture"></param>
+        /// <param name="focusDist"></param>
+        public Camera(
+            Point3 lookFrom, 
+            Point3 lookAt, 
+            Vec3 vUp, 
+            double vfov,        // vfov = vertical field-of-view in degrees
+            double aspectRatio, 
+            double aperture, 
+            double focusDist
+        )
         {
             double theta          = Utility.DegreesToRadians(vfov);
             double h              = Math.Tan(theta / 2);
             double viewportHeight = 2.0 * h;
             double viewportWidth  = aspectRatio * viewportHeight;
 
-            w = Vec3.UnitVector(lookFrom - lookAt);
-            u = Vec3.UnitVector(Vec3.Cross(vUp, w));
-            v = Vec3.Cross(w, u);
+            _w = Vec3.UnitVector(lookFrom - lookAt);
+            _u = Vec3.UnitVector(Vec3.Cross(vUp, _w));
+            _v = Vec3.Cross(_w, _u);
 
             _origin               = lookFrom;
-            _horizontal           = focusDist * viewportWidth * u;
-            _vertical             = focusDist * viewportHeight * v;
-            _lowerLeftCorner      = _origin - _horizontal/2 - _vertical/2 - focusDist * w;
+            _horizontal           = focusDist * viewportWidth * _u;
+            _vertical             = focusDist * viewportHeight * _v;
+            _lowerLeftCorner      = _origin - _horizontal/2 - _vertical/2 - focusDist * _w;
 
-            lensRadius = aperture / 2;
+            _lensRadius = aperture / 2;
         }
 
         public Ray GetRay(double s, double t)
         {
-            Vec3 rd     = lensRadius * Vec3.RandomInUnitDisk();
-            Vec3 offSet = u * rd.X + v * rd.Y;
+            Vec3 rd     = _lensRadius * Vec3.RandomInUnitDisk();
+            Vec3 offSet = _u * rd.X + _v * rd.Y;
 
             return new Ray(_origin + offSet, _lowerLeftCorner + s * _horizontal + t * _vertical - _origin - offSet);
         }
