837a0773e82a8fa6616d3c315b67d0f7bf21d147
henripc <henriquechimetta@outlook.com>
2022-07-14 13:46:20 -0300

    Main file.
    Added parallel execution of the samplePerPixel loop.

--- RayTracing/Program.cs
+++ RayTracing/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using RayTracing;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Point3 = RayTracing.Vec3;
+using Color = RayTracing.Vec3;
 
 static HittableList RandomScene()
 {
@@ -17,7 +24,7 @@ static HittableList RandomScene()
 
             if ((center - new Point3(4, 0.2, 0)).Length() > 0.9)
             {
-                Material sphereMaterial;
+                IMaterial sphereMaterial;
 
                 if (chooseMat < 0.8)
                 {
@@ -56,19 +63,22 @@ static HittableList RandomScene()
     return world;
 }
 
-static Color RayColor(ref Ray r, ref HittableList world, int depth)
+static Color RayColor(Ray r, HittableList world, int depth)
 {
     var rec = new HitRecord();
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0) return new Color(0, 0, 0);
 
-    if (world.Hit(r, 0.001, Utility.INFINITY, ref rec))
+    if (world.Hit(r, 0.001, Utility.INFINITY, rec))
     {
         var scattered = new Ray();
         var attenuation = new Color();
 
-        if (rec.mat.Scatter(r, rec, ref attenuation, ref scattered)) return attenuation * RayColor(ref scattered, ref world, depth - 1);
+        if (rec.mat!.Scatter(r, rec, attenuation, scattered))
+        {
+            return attenuation * RayColor(scattered, world, depth - 1);
+        }
 
         return new Color(0, 0, 0);
     }
@@ -82,7 +92,7 @@ static Color RayColor(ref Ray r, ref HittableList world, int depth)
 // Image
 
 const double aspectRatio  = 3.0 / 2.0;
-const int imageWidth      = 1200;
+const int imageWidth      = 400;
 const int imageHeight     = (int)(imageWidth / aspectRatio);
 const int samplesPerPixel = 100;
 const int maxDepth        = 50;
@@ -100,21 +110,42 @@ double aperture    = 0.1;
 var cam = new Camera(lookFrom, lookAt, vUp, 20, aspectRatio, aperture, distToFocus);
 
 // Render
-
-Console.WriteLine($"P3\n{ imageWidth } { imageHeight }\n255");
+var stringBuilder = new StringBuilder();
+stringBuilder.Append($"P3\n{ imageWidth } { imageHeight }\n255\n");
 
 for (int j = imageHeight - 1; j >= 0; j--)
 {
+    Console.Error.WriteLine($"Scanlines remaining: {j}");
     for (int i = 0; i < imageWidth; i++)
     {
-        var pixelColor = new Color(0, 0, 0);
-        for (int s = 0; s < samplesPerPixel; s++)
+        // Single Thread
+        //var pixelColor = new Color(0, 0, 0);
+        //for (int s = 0; s < samplesPerPixel; s++)
+        //{
+        //    double u = (i + Utility.RandomDouble()) / (imageWidth - 1);
+        //    double v = (j + Utility.RandomDouble()) / (imageHeight - 1);
+        //    Ray r = cam.GetRay(u, v);
+        //    pixelColor += RayColor(r, world, maxDepth);
+        //}
+
+        // Parallel
+        var taskResult = await Task.WhenAll(Enumerable.Range(0, samplesPerPixel).Select(_ =>
         {
-            double u = (i + Utility.RandomDouble()) / (imageWidth - 1);
-            double v = (j + Utility.RandomDouble()) / (imageHeight - 1);
-            Ray r = cam.GetRay(u, v);
-            pixelColor += RayColor(ref r, ref world, maxDepth);
-        }
-        Color.WriteColor(pixelColor, samplesPerPixel);
+            return Task.Run(() =>
+            {
+                double u = (i + Utility.RandomDouble()) / (imageWidth - 1);
+                double v = (j + Utility.RandomDouble()) / (imageHeight - 1);
+                Ray r = cam.GetRay(u, v);
+
+                return RayColor(r, world, maxDepth);
+            });
+        }));
+
+        Color pixelColor = taskResult.Aggregate((acc, color) => acc + color);
+        stringBuilder.Append(ColorUtils.WriteColor(pixelColor, samplesPerPixel));
     }
 }
+
+File.WriteAllText("./images/final_scene_parallel.ppm", stringBuilder.ToString());
+
+Console.Error.WriteLine("Done.");
