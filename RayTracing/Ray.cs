5c4e5cf3fd9f728be4ddc937f91badaa56b4bccc
henripc <henriquechimetta@outlook.com>
2022-07-16 11:48:46 -0300

    Update Ray class.
    Added Time property.

--- RayTracing/Ray.cs
+++ RayTracing/Ray.cs
@@ -17,16 +17,22 @@ namespace RayTracing
         /// </summary>
         public Vec3 Direction { get; set; }
 
+        /// <summary>
+        /// Time that the <see cref="Ray"/> exists.
+        /// </summary>
+        public double Time { get; set; }
+
         public Ray()
         {
             Origin = new Point3();
             Direction = new Vec3();
         }
 
-        public Ray(Point3 origin, Vec3 direction)
+        public Ray(Point3 origin, Vec3 direction, double time = 0d)
         {
             Origin = origin;
             Direction = direction;
+            Time = time;
         }
 
         public Point3 At(double t) => Origin + t * Direction;
