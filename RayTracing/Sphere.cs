ec92d2023fa58cda05d645c498f61cb1438df0fb
henripc <henriquechimetta@outlook.com>
2022-07-18 12:03:18 -0300

    Update the Sphre class.
    Added the GetSphereUV static method.
    Given a point in a sphere of radius one returns the spherical coordinates u and v of that point.

--- RayTracing/Sphere.cs
+++ RayTracing/Sphere.cs
@@ -49,6 +49,7 @@ namespace RayTracing
             rec.p = r.At(rec.t);
             Vec3 outwardNormal = (rec.p - Center) / Radius;
             rec.SetFaceNormal(r, outwardNormal);
+            GetSphereUV(outwardNormal, out rec.u, out rec.v);
             rec.mat = Material;
 
             return true;
@@ -62,6 +63,26 @@ namespace RayTracing
             outputBox.Maximum = tempBox.Maximum;
 
             return true;
+        }
+
+        /// <summary>
+        /// Utility function that takes points on the unit sphere centered at the origin,
+        /// and computes the spherical coordinates <paramref name="u"/> and <paramref name="v"/>.
+        /// </summary>
+        private static void GetSphereUV(Point3 p, out double u, out double v)
+        {
+            // p: a given point on the sphere of radius one, centered at the origin.
+            // u: returned value [0,1] of angle around the Y axis from X=-1.
+            // v: returned value [0,1] of angle from Y=-1 to Y=+1.
+            // <1 0 0> yields <0.50 0.50> <-1 0 0> yields <0.00 0.50>
+            // <0 1 0> yields <0.50 1.00> < 0 -1 0> yields <0.50 0.00>
+            // <0 0 1> yields <0.25 0.50> < 0 0 -1> yields <0.75 0.50>
+
+            double theta = Math.Acos(-p.Y);
+            double phi = Math.Atan2(-p.Z, p.X) + Utility.PI;
+
+            u = phi / (2 * Utility.PI);
+            v = theta / Utility.PI;
         }
     }
 }
