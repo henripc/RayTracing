e5e74159d36ca860f0757b33b0d6ade57276badf
henripc <henriquechimetta@outlook.com>
2022-07-18 11:50:03 -0300

    Create SolidColor class.
    Create a texture that represents a solid color.

---
+++ RayTracing/SolidColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Color = RayTracing.Vec3;
+
+namespace RayTracing
+{
+    /// <summary>
+    /// <see langword="class"/> that represents a solid <see cref="Color"/> texture.
+    /// </summary>
+    public class SolidColor : ITexture
+    {
+        private readonly Color _colorValue;
+
+        public SolidColor() : this(new Color(0, 0, 0)) { }
+        public SolidColor(Color color) => _colorValue = color;
+        public SolidColor(double red, double green, double blue) : this(new Color(red, green, blue)) { }
+        
+        public Color Value(double u, double v, Color p)
+        {
+            return _colorValue;
+        }
+    }
+}
