f47a00317a1f76627b2bf368dc551a9e4e1dc065
henripc <henriquechimetta@outlook.com>
2022-07-18 11:49:12 -0300

    Create Texture interface.

---
+++ RayTracing/ITexture.cs
@@ -0,0 +1,20 @@
+﻿using Color = RayTracing.Vec3;
+using Point3 = RayTracing.Vec3;
+
+namespace RayTracing
+{
+    /// <summary>
+    /// <see langword="interface"/> that represents a texture.
+    /// </summary>
+    public interface ITexture
+    {
+        /// <summary>
+        /// Returns the <see cref="Color"/> for the given coordinates.
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        Color Value(double u, double v, Point3 p);
+    }
+}
