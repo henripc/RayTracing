de9844756ecb427c22c69db90b1d4bee2e327983
henripc <henriquechimetta@outlook.com>
2022-07-18 11:54:34 -0300

    Create the CheckerTexture class.
    This class represents a checker texture.

---
+++ RayTracing/CheckerTexture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Color = RayTracing.Vec3;
+
+namespace RayTracing
+{
+    /// <summary>
+    /// <see langword="class"/> that represents a Checker texture.
+    /// </summary>
+    public class CheckerTexture : ITexture
+    {
+        public ITexture Even;
+        public ITexture Odd;
+
+        public CheckerTexture(ITexture even, ITexture odd)
+        {
+            Even = even;
+            Odd = odd;
+        }
+
+        public CheckerTexture(Color c1, Color c2)
+        {
+            Even = new SolidColor(c1);
+            Odd = new SolidColor(c2);
+        }
+
+        public Color Value(double u, double v, Color p)
+        {
+            double sines = Math.Sin(10 * p.X) * Math.Sin(10 * p.Y) * Math.Sin(10 * p.Z);
+
+            if (sines < 0)
+                return Odd.Value(u, v, p);
+            else
+                return Even.Value(u, v, p);
+        }
+    }
+}
