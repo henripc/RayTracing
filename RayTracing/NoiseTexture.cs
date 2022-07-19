67e34d577008253d7277b7ed45a8202ff3b351a4
henripc <henriquechimetta@outlook.com>
2022-07-19 12:20:29 -0300

    Create NoiseTexture class.
    Class that represents a noisy texture.

---
+++ RayTracing/NoiseTexture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Point3 = RayTracing.Vec3;
+using Color = RayTracing.Vec3;
+
+namespace RayTracing
+{
+    /// <summary>
+    /// Represents a noise <see cref="ITexture"/>.
+    /// </summary>
+    public class NoiseTexture : ITexture
+    {
+        public Perlin Noise = new Perlin();
+        public double Scale;
+
+        public NoiseTexture() { }
+        public NoiseTexture(double scale) => Scale = scale;
+
+        public Color Value(double u, double v, Point3 p)
+        {
+            return new Color(1, 1, 1) * 0.5 * (1 + Math.Sin(Scale * p.Z + 10 * Noise.Turbulance(p)));
+        }
+    }
+}
