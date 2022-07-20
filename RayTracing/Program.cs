d8d701293dd94874746941476bcdbf9e84a0ef76
henripc <henriquechimetta@outlook.com>
2022-07-20 11:17:22 -0300

    Program adapted.

--- RayTracing/Program.cs
+++ RayTracing/Program.cs
@@ -88,39 +88,68 @@ static HittableList TwoPerlinSpheres()
     return objects;
 }
 
-static Color RayColor(Ray r, HittableList world, int depth)
+static HittableList SimpleLight()
+{
+    var objects = new HittableList();
+
+    var perText = new NoiseTexture(4);
+    objects.Add(new Sphere(new Point3(0, -1000, 0), 1000, new Lambertian(perText)));
+    objects.Add(new Sphere(new Point3(0, 2, 0), 2, new Lambertian(perText)));
+
+    var diffLight = new DiffuseLight(new Color(4, 4, 4));
+    objects.Add(new RectangleXY(3, 5, 1, 3, -2, diffLight));
+
+    return objects;
+}
+
+static HittableList CornerllBox()
+{
+    var objects = new HittableList();
+
+    var red = new Lambertian(new Color(0.65, 0.05, 0.05));
+    var white = new Lambertian(new Color(0.73, 0.73, 0.73));
+    var green = new Lambertian(new Color(0.12, 0.45, 0.15));
+    var light = new DiffuseLight(new Color(15, 15, 15));
+
+    objects.Add(new RectangleYZ(0, 555, 0, 555, 555, green));
+    objects.Add(new RectangleYZ(0, 555, 0, 555, 0, red));
+    objects.Add(new RectangleXZ(213, 343, 227, 332, 554, light));
+    objects.Add(new RectangleXZ(0, 555, 0, 555, 0, white));
+    objects.Add(new RectangleXZ(0, 555, 0, 555, 555, white));
+    objects.Add(new RectangleXY(0, 555, 0, 555, 555, white));
+
+    return objects;
+}
+
+static Color RayColor(Ray r, Color background, HittableList world, int depth)
 {
     var rec = new HitRecord();
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
-    if (depth <= 0) return new Color(0, 0, 0);
-
-    if (world.Hit(r, 0.001, Utility.INFINITY, rec))
-    {
-        var scattered = new Ray();
-        var attenuation = new Color();
-
-        if (rec.mat!.Scatter(r, rec, attenuation, scattered))
-        {
-            return attenuation * RayColor(scattered, world, depth - 1);
-        }
-
+    if (depth <= 0)
         return new Color(0, 0, 0);
-    }
 
-    Vec3 unitDirection = Vec3.UnitVector(r.Direction);
-    double t = 0.5 * (unitDirection.Y + 1.0);
+    // If the ray hits nothing, return the background color.
+    if (!world.Hit(r, 0.001, Utility.INFINITY, rec))
+        return background;
 
-    return (1.0 - t) * new Color(1.0, 1.0, 1.0) + t * new Color(0.5, 0.7, 1.0);
+    var scattered = new Ray();
+    var attenuation = new Color();
+    Color emitted = rec!.mat!.Emitted(rec.u, rec.v, rec.p!);
+
+    if (!rec.mat.Scatter(r, rec, attenuation, scattered))
+        return emitted;
+
+    return emitted + attenuation * RayColor(scattered, background, world, depth - 1);
 }
 
 // Image
 
-const double aspectRatio  = 16.0 / 9.0;
-const int imageWidth      = 400;
-const int imageHeight     = (int)(imageWidth / aspectRatio);
-const int samplesPerPixel = 100;
-const int maxDepth        = 50;
+double aspectRatio  = 16.0 / 9.0;
+int imageWidth      = 400;
+int imageHeight     = (int)(imageWidth / aspectRatio);
+int samplesPerPixel = 100;
+const int maxDepth  = 50;
 
 // World
 HittableList world;
@@ -129,11 +158,13 @@ Point3 lookFrom;
 Point3 lookAt;
 double vFov     = 40.0;
 double aperture = 0;
+var background = new Color(0, 0, 0);
 
 switch (0)
 {
     case 1:
         world = RandomScene();
+        background = new Color(0.7, 0.8, 1);
         lookFrom = new Point3(13, 2, 3);
         lookAt = new Point3(0, 0, 0);
         vFov = 20.0;
@@ -142,17 +173,39 @@ switch (0)
 
     case 2:
         world = TwoSpheres();
+        background = new Color(0.7, 0.8, 1);
         lookFrom = new Point3(13, 2, 3);
         lookAt = new Point3(0, 0, 0);
         vFov = 20.0;
         break;
 
-    default:
     case 3:
         world = TwoPerlinSpheres();
+        background = new Color(0.7, 0.8, 1);
         lookFrom = new Point3(13, 2, 3);
         lookAt = new Point3(0, 0, 0);
         vFov = 20.0;
+        break;
+
+    case 5:
+        world = SimpleLight();
+        samplesPerPixel = 400;
+        background = new Color(0, 0, 0);
+        lookFrom = new Point3(26, 3, 6);
+        lookAt = new Point3(0, 2, 0);
+        vFov = 20.0;
+        break;
+
+    default:
+    case 6:
+        world = CornerllBox();
+        aspectRatio = 1d;
+        imageWidth = 600;
+        samplesPerPixel = 200;
+        background = new Color(0, 0, 0);
+        lookFrom = new Point3(278, 278, -800);
+        lookAt = new Point3(278, 278, 0);
+        vFov = 40.0;
         break;
 }
 
@@ -180,7 +233,7 @@ for (int j = imageHeight - 1; j >= 0; j--)
                 double v = (j + Utility.RandomDouble()) / (imageHeight - 1);
                 Ray r = cam.GetRay(u, v);
 
-                return RayColor(r, world, maxDepth);
+                return RayColor(r, background, world, maxDepth);
             });
         }));
 
@@ -189,6 +242,6 @@ for (int j = imageHeight - 1; j >= 0; j--)
     }
 }
 
-File.WriteAllText("./images/book2/image_10.ppm", stringBuilder.ToString());
+File.WriteAllText("./images/book2/image_12.ppm", stringBuilder.ToString());
 
 Console.Error.WriteLine("Done.");
