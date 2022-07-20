e6a4aeae05e3d2579bca049fe9ac93be24e838e1
henripc <henriquechimetta@outlook.com>
2022-07-20 11:13:08 -0300

    Create DiffuseLight class.
    Added a material that represents light source.

---
+++ RayTracing/DiffuseLight.cs
@@ -0,0 +1,25 @@
+﻿using Color = RayTracing.Vec3;
+
+namespace RayTracing
+{
+    /// <summary>
+    /// <see cref="IMaterial"/> that emits light.
+    /// </summary>
+    public class DiffuseLight : IMaterial
+    {
+        public ITexture Emit;
+
+        public DiffuseLight(ITexture texture) => Emit = texture;
+        public DiffuseLight(Color color) => Emit = new SolidColor(color);
+
+        public bool Scatter(Ray rIn, HitRecord rec, Vec3 attenuation, Ray scattered)
+        {
+            return false;
+        }
+
+        public Color Emitted(double u, double v, Color p)
+        {
+            return Emit.Value(u, v, p);
+        }
+    }
+}
